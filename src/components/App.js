08a3484a01299ca1183b4d76ba29f13afd46206c
Nobert-Muma <mumanobert17@gmail.com>
2024-07-30 13:59:02 +0300

    Wrote basic React components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ReactDOM from "react-dom";
 function NavBar() {
   return (
     <nav>
@@ -17,14 +17,22 @@ function Home() {
 }
 
 {/* write an <About> component here */}
-
+function About(){
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
